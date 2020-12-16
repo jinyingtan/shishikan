ac6f95aa48491dc8f2e65477099e6d34c3e81ecb
Marcus Koh <kohchihao@gmail.com>
2020-12-16 15:41:10 +0800

    Run prettier

--- src/components/topNavbar/index.js
+++ src/components/topNavbar/index.js
@@ -88,7 +88,7 @@ const TopNavbar = () => {
             <Button variant="ghost">Nearby</Button>
             <Button variant="ghost">MyLists</Button>
           </HStack>
-          <Divider orientation="vertical" display={{ base: 'none', md: 'flex' }}/>
+          <Divider orientation="vertical" display={{ base: 'none', md: 'flex' }} />
         </>
       ) : null}
 
