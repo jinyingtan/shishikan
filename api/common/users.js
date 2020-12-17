529f3ec356de20d7a3757c9507c20f3b548083e6
Marcus Koh <kohchihao@gmail.com>
2020-12-17 11:36:19 +0800

    Add getLists api

--- api/common/users.js
+++ api/common/users.js
@@ -21,3 +21,12 @@ export const getSharedUserData = async () => {
 
   return userData;
 };
+
+export const getCurrentUser = (auth) => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = firebaseAuth.onAuthStateChanged((user) => {
+      unsubscribe();
+      resolve(user);
+    }, reject);
+  });
+};
