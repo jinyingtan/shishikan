62b988a7ffabdb0409c41ee1fe0ead3e577639d6
Marcus Koh <kohchihao@gmail.com>
2021-01-14 00:21:51 +0800

    Add key for google places searches

--- src/components/search/Searchbar.js
+++ src/components/search/Searchbar.js
@@ -88,7 +88,7 @@ const Searchbar = ({ ...rest }) => {
                         <VStack align="stretch">
                           {suggestions.map((suggestion) => {
                             return (
-                              <HStack _hover={{ cursor: 'pointer', backgroundColor: 'gray.100' }}>
+                              <HStack key={suggestion.placeId} _hover={{ cursor: 'pointer', backgroundColor: 'gray.100' }}>
                                 <Icon as={IoLocationOutline} />
                                 <Text px="2" {...getSuggestionItemProps(suggestion)} mt="0px">
                                   {suggestion.description}
