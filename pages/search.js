f708e7de3a71b94d14131b2c201bfa9015a382fe
Marcus Koh <kohchihao@gmail.com>
2021-01-13 12:47:48 +0800

    Fix empty search page (#31)

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import SearchPage from '@components/search/pages/SearchPage';
+import { AuthProvider } from '@components/auth';
+import { verifyToken, getUserFromToken } from '@utils/auth';
 
-const Search = () => {
-  return <SearchPage />;
+export async function getServerSideProps(ctx) {
+  try {
+    const token = await verifyToken(ctx);
+    if (!token) {
+      ctx.res.writeHead(302, { Location: '/' });
+      ctx.res.end();
+    }
+    const user = getUserFromToken(token);
+
+    return {
+      props: {
+        user: user || null,
+      },
+    };
+  } catch (error) {
+    return { props: {} };
+  }
+}
+
+const Search = ({ user }) => {
+  return (
+    <AuthProvider user={user}>
+      <SearchPage />
+    </AuthProvider>
+  );
 };
 
 export default Search;
